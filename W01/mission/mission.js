a24007be397c517aacc29ada208946c15ee947e7
douglaslaursen <douglaslaursen@gmail.com>
2026-09-23 17:25:37 -0600

    Finished week 2 prove Mission statement part 2 might wait to submit till after class to make sure everything looks good.

--- W01/mission/mission.js
+++ W01/mission/mission.js
@@ -7,11 +7,33 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        // code for changes to colors and logo
+        // code for changes to colors and logo (Dark mode)
+        let  body = document.querySelectorAll("p, i, li");
+        body.forEach((body) => {
+            body.style.color = "#FFFFFF";
+        });
+
+        document.body.style.backgroundColor = "#272626"
+
+        document.querySelector("#school p").style.color = "rgb(62, 119, 139)";
+
+        document.querySelector("#school p").style.borderBottom = "1px solid white";
+
+        logo.src = "images/byui-logo-white.png";
 
     } else {
-        // code for changes to colors and logo
-        
+        // code for changes to colors and logo (Light mode)
+        let  body = document.querySelectorAll("p, i, li");
+        body.forEach((body) => {
+            body.style.color = "black";
+        });
+
+        document.body.style.backgroundColor = "white"
+
+        document.querySelector("#school p").style.color = "rgb(62, 119, 139)";
+
+        document.querySelector("#school p").style.borderBottom = "1px solid black";
+
+        logo.src = "images/byui-logo-blue.webp";
     }
-}           
-                    
+}
